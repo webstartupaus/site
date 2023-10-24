09858c1f89ed4a952286805f21204ced6fb5fc1a
webstartupaus <webstartupau@outlook.com>
2023-10-25 01:07:17 +1100

    GO

--- src/components/form.js
+++ src/components/form.js
@@ -58,7 +58,7 @@ const Form = ({ button, inputs, name }) => {
     }, [inputs]);
 
     return (
-        <form onSubmit={netlifySubmit} name={name} netlify-honeypot="bot-field" data-netlify="true">
+        <form name={name} data-netlify='true' onSubmit={netlifySubmit}>
             {inputs.map((input, i) =>
                 <Input onValidate={e => validate(e, input.pattern)} val={{ ...input }} key={i} />
             )}
