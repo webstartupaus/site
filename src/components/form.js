914818cf8e84f32945012c630a513999172cdcdf
webstartupaus <webstartupau@outlook.com>
2023-10-25 00:03:29 +1100

    Frm 2

--- src/components/form.js
+++ src/components/form.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 
 const Form = ({ button, inputs }) => {
     const [formVals, setFormVals] = useState({});
